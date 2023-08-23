fb616c749bf01947615b2c06ca045c86dd509bd9
michallef <142462683+michallef@users.noreply.github.com>
2023-08-23 00:22:26 -0700

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     ##################################################
-    # Comlete your code here
+    original_str = "Python Programming"
+    sub1 = (original_str[0:6])
+    sub2 = (original_str[7:18])
+    merged_str = sub1 + sub2
+    merge_str = sub2 + " " + sub1
+    print (sub2)
+    print (sub1)
+    print (merge_str)
     ##################################################
     pass
 
